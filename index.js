c41ee121c895ed6c76c460121a8e4facae4657bc
Hanna <hannaabdulalim@gmail.com>
2025-03-31 14:29:09 -0700

    added credentials

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const port = process.env.PORT || 5000;
 const corsOptions = {
   origin: 'https://madlit-web.vercel.app', // Replace with your actual frontend URL
   methods: 'GET,POST,PUT,DELETE',
-  allowedHeaders: 'Content-Type,Authorization'
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials:true,
 };
 //for local or open to all origins
 // app.use(cors());
